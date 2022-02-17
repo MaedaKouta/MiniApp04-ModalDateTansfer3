a438178d3dbbd5d2befd9a8d41472ecfeb37776d
MaedaKouta <maedakoutaa@gmail.com>
2022-02-17 14:40:27 +0900

    commit

---
+++ MiniApp04-ModalDateTansfer3/RedViewController.swift
@@ -0,0 +1,27 @@
+//
+//  RedViewController.swift
+//  MiniApp04-ModalDateTansfer3
+//
+//  Created by 前田航汰 on 2022/02/17.
+//
+
+import UIKit
+
+class RedViewController: UIViewController {
+
+    @IBOutlet private weak var slider: UISlider!
+    @IBOutlet private weak var numTextLabel: UILabel!
+    var num = 5
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        slider.value = Float(num)
+        numTextLabel.text = "1〜10で数字を選んで"
+    }
+
+    @IBAction private func changedSlider(_ sender: Any) {
+        num = Int(slider.value)
+        numTextLabel.text = String(num)
+    }
+
+}
--- MiniApp04-ModalDateTansfer3/ViewController.swift
+++ MiniApp04-ModalDateTansfer3/ViewController.swift
@@ -9,11 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet private weak var greenTextLabel: UILabel!
+    @IBOutlet private weak var redTextLabel: UILabel!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
+    @IBAction func exitGreen(segue: UIStoryboardSegue){
+        let greenVC = segue.source as! GreenViewController
+        greenTextLabel.text = String(greenVC.num)
+    }
+
+    @IBAction func exitRed(segue: UIStoryboardSegue){
+        let redVC = segue.source as! RedViewController
+        redTextLabel.text = String(redVC.num)
+    }
 
 }
 
